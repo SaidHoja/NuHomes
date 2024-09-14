111e5c6f6d72cecbe04926d9b427250a203bc887
SaidHoja <saidhojabusiness@gmail.com>
2024-09-14 10:36:17 -0400

    new data added

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,6 @@
 import { NCData } from '$lib/db/NCData';
 export const load = async function () {
-	const data = await NCData.find({}).project({ RegionName: 1, Total: 1, _id: 0 }).toArray();
+	const data = await NCData.find({}).project({ RegionName: 1, Total: 1, _id: 0, lat:1, lng:1 }).toArray();
 	console.log('data', data);
 	return {
 		props: {
