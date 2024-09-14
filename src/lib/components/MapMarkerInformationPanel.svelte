32d828da5f40996dbb4ab0df408da5507718516f
Ben Kirkwood <bck2196@vt.edu>
2024-09-14 13:02:15 -0400

    WOW CITY INFO

--- src/lib/components/MapMarkerInformationPanel.svelte
+++ src/lib/components/MapMarkerInformationPanel.svelte
@@ -5,7 +5,12 @@
 <div class="flex items-center flex-col justify-center h-full w-full text-center text-balance">
 	<h2 class="text-2xl text-stone-500 w-3/4">
 		{selectedMapMarker
-			? selectedMapMarker?.city + ', ' + selectedMapMarker?.state
+			? selectedMapMarker.RegionName
 			: 'Select a metro area for details'}
 	</h2>
+	<div class = "border-2 border-rose-500 flex items-center flex-col justify-center h-full w-full text-center text-balance">
+		<h2 class="text-2xl text-stone-500 w-3/4">
+			WOW City info
+		</h2>
+	</div>
 </div>
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,7 +3,7 @@ export const load = async function () {
 	const data = NCData.find({})
 		.project({ RegionName: 1, Total: 1, _id: 0, lat: 1, lng: 1 })
 		.toArray();
-	// console.log('data', data);
+	//console.log('data', data);
 	return {
 		NCData: data
 	};
