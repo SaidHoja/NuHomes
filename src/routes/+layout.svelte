510f4c972cfbe90aecaa3e64e2efafe0a486d83c
Ben Kirkwood <bck2196@vt.edu>
2024-09-14 16:16:56 -0400

    Front Picture and Text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,8 +8,8 @@
 <Separator />
 
 <div class="hero-section">
-	<div class="hero-content">
-	  <h1 class="homes-title">Find a home in a neighborhood you love.</h1>
+	<div class="bg-transparent text-6xl">
+	  <h1 class="bg-transparent">Looking for a <span style="color:orange; font-weight: bold">Nu</span> place to live?</h1>
 	  <div id="vue-homepage-typeahead" class="typeahead-container"></div>
 	</div>
   </div>
@@ -25,21 +25,6 @@
 	justify-content: center; /* Horizontally centers the content */
 	color: white; /* Text color, change as needed */
 	text-align: center; /* Center text alignment */
-  }
-  
-  .hero-content {
-	position: relative;
-	z-index: 1; /* Ensures text is above the background */
-	background-color: rgba(0, 0, 0, 0.5); /* Optional: Semi-transparent background for better readability */
-	padding: 20px; /* Optional: Add padding for spacing */
-	border-radius: 8px; /* Optional: Rounded corners */
-  }
-  
-  .homes-title {
-	margin: 0; /* Remove default margins */
-	font-size: 2rem; /* Adjust font size as needed */
-	border: none; /* Remove border */
-	background: transparent; 
   }
   
   .typeahead-container {
