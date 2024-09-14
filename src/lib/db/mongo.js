87c7fddbecdeb2bb3ae37f12b1416a4cb71c0d23
SaidHoja <saidhojabusiness@gmail.com>
2024-09-14 00:46:07 -0400

    added mongodb

---
+++ src/lib/db/mongo.js
@@ -0,0 +1,12 @@
+import {MongoClient} from 'mongodb';
+import {MONGODB_URL} from '$env/static/private';
+
+const client = new MongoClient(MONGODB_URL)
+
+export function start_mongo() {
+	console.log(MONGODB_URL);
+	console.log('Starting mongo...');
+	return client.connect();
+}
+
+export default client.db()
