650f4833ba175c953fed55cda8a20ebcbb653f02
SaidHoja <saidhojabusiness@gmail.com>
2024-09-14 09:32:39 -0400

    prettied it

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,10 @@
-import { NCData} from '$lib/db/NCData'
-export const load = async function(){
-    const data = await NCData.find({}).project({RegionName:1, Total:1, _id:0}).toArray();
-    console.log('data', data);
-    return {
-        props: {
-            NCData: JSON.stringify(data)
-    }
-    }
-}
+import { NCData } from '$lib/db/NCData';
+export const load = async function () {
+	const data = await NCData.find({}).project({ RegionName: 1, Total: 1, _id: 0 }).toArray();
+	console.log('data', data);
+	return {
+		props: {
+			NCData: JSON.stringify(data)
+		}
+	};
+};
