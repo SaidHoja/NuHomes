e81daf71dc20726e282e143c565aa588a71ab7d7
Max Rountree <max.r.rountree@gmail.com>
2024-09-15 04:11:27 -0400

    Another search bar behavior change

--- src/lib/components/InteractiveMap.svelte
+++ src/lib/components/InteractiveMap.svelte
@@ -201,7 +201,7 @@
 			{#if userSearchInput.length > 0 && searchFocused}
 				{#each searchResults as result, i}
 					<button
-						class={`text-sm p-2 transition-all hover:bg-gray-100 ${i == 2 || searchResults.length == 1 ? 'rounded-b-lg' : ''} w-full text-left pl-3 ${i == searchResultsCursor && !searchMouseEntered ? 'bg-gray-100' : ''}`}
+						class={`text-sm p-2 transition-all hover:bg-gray-100 ${i == 1 || searchResults.length == 1 ? 'rounded-b-lg' : ''} w-full text-left pl-3 ${i == searchResultsCursor && !searchMouseEntered ? 'bg-gray-100' : ''}`}
 						on:click={() => {
 							// userSearchInput = result.RegionName;
 							// map.flyToBounds(result.circle);
