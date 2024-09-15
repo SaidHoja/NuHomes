629c8a7f473a065026823aa028dd478cdd00eff1
SaidHoja <saidhojabusiness@gmail.com>
2024-09-15 04:30:22 -0400

    added markdown support

--- src/lib/components/ChatPanel.svelte
+++ src/lib/components/ChatPanel.svelte
@@ -18,7 +18,70 @@
 			content: `Hi, I'm Nubert! Ask me any question you might have about ${city}!`
 		};
 	}
+	// attribution: https://dev.to/casualwriter/a-simple-markdown-parser-in-50-lines-of-js-4gpi
+	function simpleMarkdown(mdText) {
+		// first, handle syntax for code-block
+		mdText = mdText.replace(/\r\n/g, '\n');
+		mdText = mdText.replace(
+			/\n~~~ *(.*?)\n([\s\S]*?)\n~~~/g,
+			'<pre><code title="$1">$2</code></pre>'
+		);
+		mdText = mdText.replace(
+			/\n``` *(.*?)\n([\s\S]*?)\n```/g,
+			'<pre><code title="$1">$2</code></pre>'
+		);
 
+		// split by "pre>", skip for code-block and process normal text
+		var mdHTML = '';
+		var mdCode = mdText.split('pre>');
+
+		for (var i = 0; i < mdCode.length; i++) {
+			if (mdCode[i].substr(-2) == '</') {
+				mdHTML += '<pre>' + mdCode[i] + 'pre>';
+			} else {
+				mdHTML += mdCode[i]
+					.replace(/(.*)<$/, '$1')
+					.replace(/^##### (.*?)\s*#*$/gm, '<h5>$1</h5>')
+					.replace(/^#### (.*?)\s*#*$/gm, '<h4 id="$1">$1</h4>')
+					.replace(/^### (.*?)\s*#*$/gm, '<h3 id="$1">$1</h3>')
+					.replace(/^## (.*?)\s*#*$/gm, '<h2 id="$1">$1</h2>')
+					.replace(/^# (.*?)\s*#*$/gm, '<h1 id="$1">$1</h1>')
+					.replace(/^-{3,}|^\_{3,}|^\*{3,}/gm, '<hr/>')
+					.replace(/``(.*?)``/gm, '<code>$1</code>')
+					.replace(/`(.*?)`/gm, '<code>$1</code>')
+					.replace(/^\>> (.*$)/gm, '<blockquote><blockquote>$1</blockquote></blockquote>')
+					.replace(/^\> (.*$)/gm, '<blockquote>$1</blockquote>')
+					.replace(/<\/blockquote\>\n<blockquote\>/g, '\n<br>')
+					.replace(/<\/blockquote\>\n<br\><blockquote\>/g, '\n<br>')
+					.replace(/!\[(.*?)\]\((.*?) "(.*?)"\)/gm, '<img alt="$1" src="$2" $3 />')
+					.replace(/!\[(.*?)\]\((.*?)\)/gm, '<img alt="$1" src="$2" />')
+					.replace(/\[(.*?)\]\((.*?) "(.*?)"\)/gm, '<a href="$2" title="$3">$1</a>')
+					.replace(/<http(.*?)\>/gm, '<a href="http$1">http$1</a>')
+					.replace(/\[(.*?)\]\(\)/gm, '<a href="$1">$1</a>')
+					.replace(/\[(.*?)\]\((.*?)\)/gm, '<a href="$2">$1</a>')
+					.replace(/^[\*|+|-][ |.](.*)/gm, '<ul><li>$1</li></ul>')
+					.replace(/<\/ul\>\n<ul\>/g, '\n')
+					.replace(/^\d[ |.](.*)/gm, '<ol><li>$1</li></ol>')
+					.replace(/<\/ol\>\n<ol\>/g, '\n')
+					.replace(/\*\*\*(.*)\*\*\*/gm, '<b><em>$1</em></b>')
+					.replace(/\*\*(.*)\*\*/gm, '<b>$1</b>')
+					.replace(/\*([\w \d]*)\*/gm, '<em>$1</em>')
+					.replace(/___(.*)___/gm, '<b><em>$1</em></b>')
+					.replace(/__(.*)__/gm, '<u>$1</u>')
+					.replace(/_([\w \d]*)_/gm, '<em>$1</em>')
+					.replace(/~~(.*)~~/gm, '<del>$1</del>')
+					.replace(/\^\^(.*)\^\^/gm, '<ins>$1</ins>')
+					.replace(/ +\n/g, '\n<br/>')
+					.replace(/\n\s*\n/g, '\n<p>\n')
+					.replace(/^ {4,10}(.*)/gm, '<pre><code>$1</code></pre>')
+					.replace(/^\t(.*)/gm, '<pre><code>$1</code></pre>')
+					.replace(/<\/code\><\/pre\>\n<pre\><code\>/g, '\n')
+					.replace(/\\([`_\\\*\+\-\.\(\)\[\]\{\}])/gm, '$1');
+			}
+		}
+
+		return mdHTML.trim();
+	}
 	const USER_ID = 'user';
 	const LLM_ID = 'assistant';
 	const FIRST_ID = 'system';
@@ -36,7 +99,9 @@
 	const customEnhanced = ({ formElement, formData, action, cancel, submitter }) => {
 		// console.log(formData.keys())
 		const existingMessages = JSON.parse(formData.get('messages'));
-		const newMessage = formData.get('newMessage');
+		let newMessage = formData.get('newMessage');
+		newMessage = simpleMarkdown(newMessage);
+		console.log(newMessage);
 		userInput = '';
 		replyPending = true;
 
@@ -48,7 +113,7 @@
 			if (result.status == 200) {
 				if (result.data.success) {
 					messages[messages.length - 1].confirmed = true;
-					messages = [...messages, { role: LLM_ID, content: result.data.reply, confirmed: true }];
+					messages = [...messages, { role: LLM_ID, content: simpleMarkdown(result.data.reply), confirmed: true }];
 					return;
 				}
 			}
@@ -73,7 +138,11 @@
 			.filter((a) => a.role != FIRST_ID)
 			.reverse()
 			.concat([placeholderFirstMessage]) as message}
-			<ChatMessage {message}>{message.content}</ChatMessage>
+			{#if message.role === LLM_ID}
+				<ChatMessage {message}>{@html message.content}</ChatMessage>
+			{:else}
+				<ChatMessage {message}>{message.content}</ChatMessage>
+			{/if}
 		{/each}
 	</div>
 	<form
