579b804d45139fd00bde9ad0e1222dd8d645a0ff
SaidHoja <saidhojabusiness@gmail.com>
2024-09-14 21:49:34 -0400

    got ai description for city working

--- src/routes/api/chatbot/+server.js
+++ src/routes/api/chatbot/+server.js
@@ -4,13 +4,12 @@ export const POST = async ({ request: req }) => {
 	// Extract the `messages` from the body of the request
 
 	let { regionName } = await req.json();
-  regionName = "asdf"
 	console.log(regionName);
 	//Request the OpenAI API for the response based on the prompt
-  let messages = [{ role: 'user', content: 'Give me a 100 word summary about ' + regionName }];
+  let messages = [{ role: 'user', content: 'Give me a 50 word summary about ' + regionName }];
   let summary = await messagePPLX(messages, 0.0);
   console.log("this is the message" , summary)
 
-  return new Response(JSON.stringify({ status: 200}, {message : summary}))
+  return new Response(JSON.stringify({ status: 200 , message : summary}))
 	
 };
